78c677ec899a07471635b21a9ba4eeb19a073e4c
Luis Gonzalez <lgonzalez@ioet.com>
2019-07-03 23:38:29 -0500

    Firebase login/register

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 import Vue from 'nativescript-vue'
-import App from './components/App'
+import LoginPage from './components/LoginPage'
 import store from './store';
 
 import VueDevtools from 'nativescript-vue-devtools'
@@ -12,7 +12,22 @@ Vue.config.silent = (TNS_ENV === 'production')
 
 Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
 
+var firebase = require("nativescript-plugin-firebase");
+firebase
+  .init({
+    // Optionally pass in properties for database, authentication and cloud messaging,
+    // see their respective docs.
+  })
+  .then(
+    function(instance) {
+      console.log("firebase.init done");
+    },
+    function(error) {
+      console.log("firebase.init error: " + error);
+    }
+  );
+
 new Vue({
   store,
-  render: h => h('frame', [h(App)])
+  render: h => h('frame', [h(LoginPage)])
 }).$start()
